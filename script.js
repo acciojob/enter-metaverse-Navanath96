d542e0a2a653181ab07e332127cf352811b871f4
Navanath96 <117847388+Navanath96@users.noreply.github.com>
2023-06-15 00:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let p=document.getElementById("status");
+// console.log(p.innerText);
+let btn=document.getElementById("enterBtn");
+let div=document.getElementById("div");
+btn.addEventListener("click",change);
+function change(){
+    // console.log("hii");
+    p.remove();
+    let h1=document.createElement("h1");
+    h1.innerText="Entered Metaverse";
+    div.appendChild(h1);
+}
